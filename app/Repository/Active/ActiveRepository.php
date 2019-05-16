ef49c9739d1e590951bd7db83153f47d4070b5ff
chuangying2017 <631711487@qq.com>
2019-05-16 23:27:22 +0800

    数据字段增加

--- app/Repository/Active/ActiveRepository.php
+++ app/Repository/Active/ActiveRepository.php
@@ -39,7 +39,7 @@ class ActiveRepository
 
         unset($arr['createTime']);
 
-      //  $arr['create_at'] = date('Y-m-d H:i:s',$createTime);
+        $arr['created_at'] = date('Y-m-d H:i:s',$createTime);
 
         if ($ariseNum > 0)
         {
@@ -61,7 +61,9 @@ class ActiveRepository
 
             $res = MaCategory::create([
                 'title' => $category
-            ])->active()->create($arr);
+            ]);
+            $arr['category_id'] = $res->id;
+            $res = $res ->active()->create($arr);
         }
 
         if ($res)
