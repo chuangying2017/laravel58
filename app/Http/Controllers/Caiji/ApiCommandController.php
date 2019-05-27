445caeb560d0dc9a538912399639af3c731c4096
chuangying2017 <631711487@qq.com>
2019-05-27 18:19:28 +0800

    放开通道

--- app/Http/Controllers/Caiji/ApiCommandController.php
+++ app/Http/Controllers/Caiji/ApiCommandController.php
@@ -19,7 +19,7 @@ class ApiCommandController extends Controller
         $github_signa = $_SERVER['HTTP_X_HUB_SIGNATURE'];//fbneinfie
         list($hash_type, $hash_value) = explode('=', $github_signa, 2);
         $payload = file_get_contents("php://input");
-        Storage::disk('local')->put('githubTest.log',json_encode($payload));
+        Storage::disk('local')->put('githubTest.log',serialize($payload));
         $secret = 'xiaoma1212';
         $hash = hash_hmac($hash_type,$payload,$secret);
         if($hash && $hash === $hash_value)
