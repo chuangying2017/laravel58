058874e992025d3bc7abe2aeefa7263f84ba66ea
chuangying2017 <631711487@qq.com>
2019-06-18 01:09:00 +0800

    点击复制

--- resources/views/admin/qrcode.blade.php
+++ resources/views/admin/qrcode.blade.php
@@ -1,2 +1,12 @@
-<input type="text" value="{{$location}}" />
+<script type="text/javascript" src="{{asset('h-ui/lib/jquery/1.9.1/jquery.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('h-ui/lib/layer/2.4/layer.js')}}"></script>
+<input type="text" id="copy"  value="{{$location}}" /> <input type="button" value="点击复制" onclick="copy()" />
 <img src="{{$url}}" alt=""/>
+<script type="application/javascript">
+    function copy() {
+        var e = document.getElementById("copy");
+        e.select(); // 选择对象
+        document.execCommand("Copy"); // 执行浏览器复制命令
+        layer.msg("复制成功！",{icon:1,time:1000});
+    }
+</script>
